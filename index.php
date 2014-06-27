cf9d9e78b3e526164f7bc9fdaf7a23769c593a37
Austin Lin <austin@austinylin.com>
2014-06-27 01:09:08 -0400

    Test db connection

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
-print "Hello World";
+$db = new PDO("mysql:host={$_ENV['DB_PORT_3306_TCP_ADDR']};port={$_ENV['DB_PORT_3306_TCP_PORT']};dbname={$_ENV['DB_ENV_MYSQL_DATABASE']};charset=utf8", $_ENV['DB_ENV_MYSQL_USER'], '');
+
+foreach($db->query('SELECT * FROM world') as $row) {
+  echo "Hello ".$row['name']."<br>"; //etc...
+}
+
 
 ?>
